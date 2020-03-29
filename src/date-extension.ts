7b9c3af9a31126a591e02c035de3f89b90e92073
antoniocasino <casinoantonio@gmail.com>
2020-03-29 18:47:16 +0200

    global interface Date

--- src/date-extension.ts
+++ src/date-extension.ts
@@ -1,10 +1,10 @@
 export {};
 
-/*declare global {
+declare global {
     interface Date {
         comingFrom(): string;
     }
-}*/
+}
 
 Date.prototype.comingFrom = function (): string {
     return 'this invocation of Date comes from webpack-typescript-starter project';
